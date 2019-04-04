8debf0c479668f3c8544c06fc4fb22c8044a7229
s4513760 <mirapedr@colpatria.com>
2019-04-04 08:33:20 -0500

    Mocks de películas

--- src/app/models/mocks/movie-mocks.ts
+++ src/app/models/mocks/movie-mocks.ts
@@ -1,18 +1,29 @@
 import { Movie } from '../movie.model';
+import { DIRECTORS } from './director-mocks';
 
 export const MOVIES: Movie[] = [{
   id: 1,
   title: 'Avengers Endgame',
   year: 2019,
-  rating: 4
+  rating: 4,
+  directors: [
+    DIRECTORS[2],
+    DIRECTORS[3],
+  ]
 }, {
   id: 2,
   title: 'Kill Bill 1',
   year: 2003,
-  rating: 5
+  rating: 5,
+  directors: [
+    DIRECTORS[0]
+  ]
 }, {
   id: 3,
   title: 'The Godfather',
   year: 1972,
-  rating: 5
+  rating: 5,
+  directors: [
+    DIRECTORS[1]
+  ]
 }];
