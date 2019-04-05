50cb76e708781a3a1e77c03f1338448b42c9f78a
s4513760 <mirapedr@colpatria.com>
2019-04-05 16:27:33 -0500

    Barra de título con rutas a módulos

--- src/app/components/title-bar/title-bar.component.ts
+++ src/app/components/title-bar/title-bar.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { moviesPaths, directorPaths } from 'src/app/utils/front-paths';
 
 @Component({
   selector: 'app-title-bar',
   templateUrl: './title-bar.component.html',
   styleUrls: ['./title-bar.component.css']
 })
-export class TitleBarComponent implements OnInit {
+export class TitleBarComponent {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit() {
+  public goToMovies() {
+    this.router.navigate([moviesPaths.movies]);
   }
+
+  public goToDirectors() {
+    this.router.navigate([directorPaths.directors]);
+  }
+
 
 }
